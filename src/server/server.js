3ad8a63c1d00bd248b7dede5c1ff2ec49fd92ab0
apple2 <apple2@apple2deMacBook.local>
2022-03-24 22:35:00 +0800

    second official commit

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose') 
 require('dotenv').config()
-/* database connection  */
-mongoose.connect('mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD +'@thesis.tujpf.mongodb.net/thesis_project', {
+/* database connection */
+mongoose.connect('mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD +'@thesis.tujpf.mongodb.net/'+process.env.DB_NAME, {
 useNewUrlParser:true
 } ,()=>{
   console.log('connection established succefully')
